6bb94acad6a820724bcc7a65e3e82df906007ef4
Crowdbotics <team@crowdbotics.com>
2021-08-29 23:47:40 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "fancy_violet_30183Identifier",
+  url: "https://fancy_violet_30183Identifier.botics.co",
+  api: "https://fancy_violet_30183Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
